d665043d62e1954973485573b2d7e8c19807ad62
Ishaan Shanker Srivastava <77294438+Comrade-1729@users.noreply.github.com>
2024-11-05 21:32:28 +0530

    Delete #13 Easy directory

--- #13 Easy/13_Roman2Integer.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        dict = {('I':1),('V':5),('X':10),('L':50),('C':100),('D':500),('M':1000)}
-        for i,v in enumerate(s):
-            pass
